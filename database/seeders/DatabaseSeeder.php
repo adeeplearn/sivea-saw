39c087de3b7037e592d182881f69c1d0083320b1
adeeplearn <galang8280@students.amikom.ac.id>
2021-12-21 21:59:21 +0700

    implement recent seeder on DatabaseSeeder run method

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\CriteriaRating;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,7 +17,12 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         return $this->call([
             ProdiSeeder::class,
-            MatakuliahSeeder::class
+            MatakuliahSeeder::class,
+            DosenSeeder::class,
+            AsistenSeeder::class,
+            CriteriaWeightSeeder::class,
+            SubCriteriaSeeder::class,
+            CriteriaRatingSeeder::class,
         ]);
     }
 }
