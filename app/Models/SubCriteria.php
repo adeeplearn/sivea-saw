daa4ef093656cef2a476bef8d645cfa6586aabe2
adeeplearn <galang8280@students.amikom.ac.id>
2021-12-21 19:31:25 +0700

    add subcriteria model migration

---
+++ app/Models/SubCriteria.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SubCriteria extends Model
+{
+    use HasFactory;
+
+    protected $table = 'subcriterias';
+    protected $fillable = ['criteria_id', 'poin_penilaian'];
+
+    public function criteria(){
+        return $this->hasMany(CriteriaWeight::class);
+    }
+}
---
+++ database/migrations/2021_12_21_054452_create_sub_criterias_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubCriteriasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subcriterias', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('criteria_id')
+                ->constrained('criteriaweights')
+                ->cascadeOnDelete();
+            $table->text('poin_penilaian');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('subcriterias', function (Blueprint $table) {
+            $table->dropForeign('criteria_id');
+            $table->dropColumn('criteria_id');
+        });
+        Schema::dropIfExists('subcriterias');
+    }
+}
