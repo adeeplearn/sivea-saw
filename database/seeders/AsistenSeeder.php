801ccd86b9f4436eca4e37dc2e8e19805d52b8f3
adeeplearn <galang8280@students.amikom.ac.id>
2021-12-21 19:54:57 +0700

    add AsistenSeeder DosenSeeder MengajarSeeder

---
+++ database/seeders/AsistenSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Asisten;
+use Illuminate\Database\Seeder;
+
+class AsistenSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Asisten::create(['']);
+    }
+}
--- database/seeders/ProdiSeeder.php
+++ database/seeders/ProdiSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Prodi;
 use Illuminate\Database\Seeder;
 
 class ProdiSeeder extends Seeder
