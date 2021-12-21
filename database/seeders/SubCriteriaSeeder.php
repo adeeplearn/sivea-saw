55ab1b34b6717661de7dcec239e6aed73e38ee81
adeeplearn <galang8280@students.amikom.ac.id>
2021-12-21 21:32:30 +0700

    update SubCriteriaSeeder

--- database/seeders/SubCriteriaSeeder.php
+++ database/seeders/SubCriteriaSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\CriteriaWeight;
+use App\Models\SubCriteria;
 use Illuminate\Database\Seeder;
 
 class SubCriteriaSeeder extends Seeder
@@ -13,6 +15,24 @@ class SubCriteriaSeeder extends Seeder
      */
     public function run()
     {
-        //
+        CriteriaWeight::where('name','Microteach')->first()->subcriteria()->saveMany([
+            new SubCriteria(['poin_penilaian'=>'Menilai kemampuan presentasi dan memberi penjelasan']),
+            new SubCriteria(['poin_penilaian'=>'Menilai penguasaan materi praktikum']),
+        ]);
+        CriteriaWeight::where('name','Perilaku')->first()->subcriteria()->saveMany([
+            new SubCriteria(['poin_penilaian'=>'Menilai kerapian penampilan asisten praktikum']),
+            new SubCriteria(['poin_penilaian'=>'Menilai sikap asisten terhadap dosen dan mahasiswa/praktikan']),
+        ]);
+        CriteriaWeight::where('name','Kedisiplinan')->first()->subcriteria()->saveMany([
+            new SubCriteria(['poin_penilaian'=>'Respon ketepatan waktu asisten serta kehadiran sesuai jadwal']),
+            new SubCriteria(['poin_penilaian'=>'Ketaatan sesuai peraturan asisten yang berlaku']),
+        ]);
+        CriteriaWeight::where('name','Keahlian')->first()->subcriteria()->saveMany([
+            new SubCriteria(['poin_penilaian'=>'Mengoperasikan properti lab praktikum dengan baik']),
+            new SubCriteria(['poin_penilaian'=>'Keaktifan asisten serta kontribusi yang diberikan']),
+        ]);
+        CriteriaWeight::where('name','Rekomendasi')->first()->subcriteria()->saveMany([
+            new SubCriteria(['poin_penilaian'=>'Rekomendasi asisten terbaik']),
+        ]);
     }
 }
